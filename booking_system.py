8b8dee6b75aade7afc52b2789bbadf76964fa048
carlosleite <carlosmagalhaesleite@gmail.com>
2021-05-12 23:03:59 +0100

    major refactor to include multithread - one thread per user/class included crawler

--- booking_system.py
+++ booking_system.py
@@ -1,170 +1,171 @@
+import argparse
 import datetime
 import json
+import logging
 import os
+import threading
 import time
-from typing import List, Tuple
+from pathlib import Path
+from typing import List, Dict, Any
 
 import requests
 from dateutil import parser
 
 from booking_user import BookingUser
 
+website_base_url = "https://bhmbackend.m8north.co.uk/"
+offsets = {
+    'gymClass': 55,
+    'swimmingClass': 55,
+    'tennisClass': 835
+}
+
+def init_logger(name, filename):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler(filename, mode='a')
+    fh.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('[%(asctime)s] %(levelname)s  %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    return logger
+
 
 class BookingSystem:
-    def __init__(self, users_configs: str, base_url: str):
-        with open(users_configs, 'r') as j:
-            contents = json.loads(j.read())
-        self.booking_users = [BookingUser(base_url, user_preference) for user_preference in contents]
-
-    def _auth_users(self) -> Tuple[int, List[BookingUser]]:
-        auth_users = 0
-        auth_users_list = []
-
-        for user in self.booking_users:
-            try:
-                user.auth()
-                user.me()
-                auth_users += 1
-                auth_users_list.append(user)
-                print(f"Authentication success for user {user.name}")
-            except requests.exceptions.HTTPError:
-                print(f"Authentication failed for user {user.name}")
-
-        return auth_users, auth_users_list
+    def __init__(self, base_url: str, class_type: str, user_info, logger):
+        self.base_url = base_url
+        self.user = BookingUser(base_url, class_type, user_info, logger)
+        self.logger = logger
 
     def enforce_auth(self):
         while True:
-            auth_users, auth_user_list = self._auth_users()
-            if auth_users == len(self.booking_users):
-                print("Successfully authenticated all the users")
+            try:
+                self.user.auth()
+                self.user.me()
+                self.logger.info(f"Authentication success")
                 break
-            elif auth_users > 0:
-                print("Failed authentication for some users. Skipping those users.")
-                self.booking_users = auth_user_list
-                break
-            else:
-                print("Failed authentication for all users. Sleeping 30 mins")
-
-            time.sleep(1800)
+            except requests.exceptions.HTTPError:
+                self.logger.error(f"Authentication failed for user {self.user.name}. Sleeping 30 mins")
+                time.sleep(1800)
 
     def search_target_date(self, target_last_date: str):
-        dt = datetime.datetime.now()
-        print(f"Start searching for date {target_last_date} at {dt}")
+        self.logger.info(f"Start searching for date {target_last_date}")
         start = time.time()
-        user = self.booking_users[0]
+
         while True:
             try:
                 last_dates_available = []
-                for class_type in user.classes:
-                    url = os.path.join(user.base_url, user.classes_url, class_type)
-                    r = requests.get(url, headers=user.headers, timeout=user.timeout)
-                    r.raise_for_status()
+                url = os.path.join(self.base_url, self.user.classes_url, self.user.class_type)
+                r = requests.get(url, headers=self.user.headers, timeout=self.user.timeout)
+                r.raise_for_status()
 
-                    classes = r.json()
-                    last_date = parser.parse(classes[-1]["_id"])
-                    last_dates_available.append(datetime.datetime.strftime(last_date, "%Y-%m-%d"))
+                classes = r.json()
+                last_date = parser.parse(classes[-1]["_id"])
+                last_dates_available.append(datetime.datetime.strftime(last_date, "%Y-%m-%d"))
 
                 dt = datetime.datetime.now()
                 if target_last_date in last_dates_available:
-                    print(f"Found target date available at {dt}")
+                    self.logger.info("Found target date")
                     break
                 else:
                     next_time = datetime.datetime.combine(dt, datetime.time(hour=dt.hour + 1))
                     diff = (next_time - dt)
                     secs = diff.total_seconds()
-                    print(f"Target date not available at {dt}. Sleeping {secs} seconds.")
+                    self.logger.info(f"Target date not available. Sleeping {secs} seconds.")
 
                     end = time.time()
                     if end - start >= 10800:
-                        print("3 hours have passed since we started searching for the target date. Re-authenticating.")
+                        self.logger.info("Re-auth: 3 hours have passed since we started searching for the target date")
                         self.enforce_auth()
                         start = time.time()
                     time.sleep(secs)
             except requests.exceptions.HTTPError:
-                print("Connection error searching for date. Sleeping 10min.")
-                time.sleep(600)
+                self.logger.error("Connection error searching for date. Sleeping 30min.")
+                time.sleep(1800)
                 self.enforce_auth()
 
-    def save_user_bookings(self):
-        bookings_to_save = {}
-        for user in self.booking_users:
-            scheduled_classes = user.get_scheduled_classes()
-            all_bookings = {}
+    def do_bookings(self, classes_to_schedule: List[Dict[str, Any]]):
+        for class_candidate in classes_to_schedule:
+            day = parser.parse(class_candidate["classDate"]).strftime("%Y-%m-%d")
+            self.user.book_class(class_candidate["_id"])
+            self.logger.info(f"Booked class for {day} at {class_candidate['classTime']}")
 
-            for class_type, bookings in scheduled_classes.items():
-                for booking in bookings:
-                    class_date = booking['classDate'][0]
-                    data = {"classType": class_type, "class_id": booking["class_id"],
-                            "booking_id": booking["booking_id"], "classTime": booking["classTime"]}
-                    if class_date in all_bookings.keys():
-                        all_bookings[class_date].append(data)
-                    else:
-                        all_bookings[class_date] = [data]
+    def crawler(self, candidates, tomorrow):
+        dt = datetime.datetime.now()
+        start = time.time()
+        start_logging = time.time()
+        self.logger.info(f"There are candidates not available: {candidates}")
+        self.logger.info("Crawler started")
+        while dt < tomorrow and len(candidates) > 0:
+            classes_to_schedule, candidates_not_scheduled = self.user.get_classes_to_schedule(candidates)
+            if len(classes_to_schedule) > 0:
+                self.do_bookings(classes_to_schedule)
 
-            if len(all_bookings) > 0:
-                bookings_to_save[user.name] = all_bookings
+            if len(candidates_not_scheduled) > 0:
+                candidates = candidates_not_scheduled
+                end = time.time()
+                end_logging = time.time()
 
-        d = datetime.datetime.now().strftime("%Y-%m-%d")
-        filename = f"users_data/{d}_scheduled_classes.txt"
-        with open(filename, 'w') as outfile:
-            json.dump(bookings_to_save, outfile)
+                if end - start >= 10800:
+                    self.logger.info("3 hours have passed since last auth for crawler. Re-authenticating.")
+                    self.enforce_auth()
+                    start = time.time()
 
-    def generate_candidates(self):
-        for user in self.booking_users:
-            user.update_candidates()
+                if (end_logging - start_logging) // 3600 > 0:
+                    self.logger.info("After 1h there are still classes not found. Continue crawler")
+                    start_logging = time.time()
 
-    def do_bookings(self):
-        for user in self.booking_users:
-            print("-------------------------------------------------------------------------")
+                time.sleep(60)
             dt = datetime.datetime.now()
-            print(f"Start bookings for user {user.name} at {dt}")
-            candidates = user.candidates
-            len_candidates = sum([len(x) for _, x in candidates.items()])
 
-            if len_candidates == 0:
-                print(f"All the classes are already booked for user {user.name}")
-            else:
-                for candidate_class, candidates_info in candidates.items():
-                    classes_to_schedule, cancelled_classes = user.get_classes_to_schedule(candidate_class,
-                                                                                          candidates_info)
-                    candidates_info = [c for c in candidates_info if c not in cancelled_classes]
-                    print(f"\nCandidates for {candidate_class}: {candidates_info}")
+        self.logger.info("Stopping crawler")
 
-                    if len(candidates_info) == 0:
-                        print("No candidates available")
-                    elif len(classes_to_schedule) == 0:
-                        print("All of those classes are not available")
-                    else:
-                        if len(classes_to_schedule) < len(candidates_info):
-                            candidates_temp = {(c[0], c[2]) for c in candidates_info}
-                            scheduled_temp = {(s["classDate"], s["classTime"].lower()) for s in classes_to_schedule}
-                            print(
-                                f"Some classes are not available: {candidates_temp.difference(scheduled_temp)}")
+    def run(self, offset_tomorrow: int):
+        try:
+            while True:
+                dt = datetime.datetime.now()
+                tomorrow = datetime.datetime.combine(dt + datetime.timedelta(days=1), datetime.time.min)
+                tomorrow = tomorrow + datetime.timedelta(minutes=offset_tomorrow)
+                target_last_date = dt + datetime.timedelta(days=7)
+                target_last_date = datetime.datetime.strftime(target_last_date, "%Y-%m-%d")
 
-                        dt = datetime.datetime.now()
-                        print(f"Booking class type {candidate_class} at {dt}s")
-                        for class_candidate in classes_to_schedule:
-                            day = parser.parse(class_candidate["classDate"]).strftime("%Y-%m-%d")
-                            user.book_class(class_candidate["_id"])
-                            print(f"{day} at {class_candidate['classTime']}")
+                self.enforce_auth()
+                candidates = self.user.generate_candidates()
+                self.search_target_date(target_last_date)
+                classes_to_schedule, candidates_not_available = self.user.get_classes_to_schedule(candidates)
 
-    def run(self):
-        while True:
-            dt = datetime.datetime.now()
-            target_last_date = dt + datetime.timedelta(days=7)
-            target_last_date = datetime.datetime.strftime(target_last_date, "%Y-%m-%d")
-            print(f"Start bookings for {dt.day}/{dt.month}/{dt.year} at {dt}s")
+                if len(classes_to_schedule) > 0:
+                    self.do_bookings(classes_to_schedule)
+                if len(candidates_not_available) > 0:
+                    self.crawler(candidates_not_available, tomorrow)
 
-            self.enforce_auth()
-            self.generate_candidates()
-            self.search_target_date(target_last_date)
-            self.do_bookings()
-            self.save_user_bookings()
+                dt = datetime.datetime.now()
+                if dt < tomorrow:
+                    time_until_tomorrow = tomorrow - dt
+                    self.logger.info(f"There are no candidates to book - sleep {time_until_tomorrow} until next day")
+                    time.sleep(time_until_tomorrow.seconds)
+        except Exception as e:
+            self.logger.exception(e)
+            raise e
 
-            dt = datetime.datetime.now()
-            tomorrow = dt + datetime.timedelta(days=1)
-            time_until_tomorrow = datetime.datetime.combine(tomorrow, datetime.time.min) + datetime.timedelta(
-                minutes=55) - dt
-            seconds = time_until_tomorrow.seconds
-            print(f"Success in bookings at {dt}, sleeping {time_until_tomorrow} until tomorrow")
-            time.sleep(seconds)
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument("--config", type=str)
+    args = p.parse_args()
+
+    config_path = args.config
+    with open(config_path, 'r') as j:
+        contents = json.loads(j.read())
+
+    for user_data in contents:
+        for class_type, class_data in user_data["preferences"].items():
+            logs_path = f"users_data/logs"
+            Path(logs_path).mkdir(parents=True, exist_ok=True)
+            logger_name = f"{user_data['name']}_{class_type}"
+            filename_log = f"{logs_path}/{logger_name}.log"
+            logger = init_logger(logger_name, filename_log)
+
+            booking_system = BookingSystem(website_base_url, class_type, user_data, logger)
+            process = threading.Thread(target=booking_system.run, args=(offsets[class_type],))
+            process.start()
--- booking_user.py
+++ booking_user.py
@@ -7,106 +7,96 @@ from dateutil import parser
 
 
 class BookingUser:
-    def __init__(self, base_url, user_info):
+    def __init__(self, base_url: str, class_type: str, user_info, logger):
         self.base_url = base_url
+        self.class_type = class_type
         self.name = user_info["name"]
         self.user_email = user_info["email"]
         self.password = user_info["password"]
-        self.user_preferences = user_info["preferences"]
+        self.user_preferences = user_info["preferences"][self.class_type]
         self.auth_url = "auth/local"
         self.classes_url = "api/class/gym/5fd7cff72eb93d371e0aa7de"
         self.headers = {"Content-Type": "application/json"}
         self.timeout = 3.0
         self.user_id = None
-        self.classes = ["swimmingClass", "gymClass", "tennisClass"]
-        self.candidates = []
+        self.logger = logger
 
-    def get_scheduled_classes(self) -> Dict[str, List[Dict[str, Any]]]:
+    def get_scheduled_classes(self) -> List[Dict[str, Any]]:
         # return scheduled classes for a given user
-        booked_classes = {key_class: [] for key_class in self.classes}
+        booked_classes = []
+        url = os.path.join(self.base_url, f"api/users/{self.class_type}/upcoming")
+        r = requests.get(url, headers=self.headers, timeout=self.timeout)
+        r.raise_for_status()
 
-        for class_type in booked_classes.keys():
-            url = os.path.join(self.base_url, f"api/users/{class_type}/upcoming")
-            r = requests.get(url, headers=self.headers, timeout=self.timeout)
-            r.raise_for_status()
-
-            for booked_class in r.json():
-                if booked_class["status"] == "active":
-                    class_tmp = dict()
-                    class_tmp["booking_id"] = booked_class["_id"]
-                    booked_class = booked_class['class']
-                    class_tmp["class_id"] = booked_class["_id"]
-                    day = parser.parse(booked_class["classDate"])
-                    class_tmp["classDate"] = (day.strftime("%Y-%m-%d"), day.strftime("%A"))
-                    class_tmp["classTime"] = booked_class["classTime"]
-                    booked_classes[class_type].append(class_tmp)
+        for booked_class in r.json():
+            if booked_class["status"] == "active":
+                class_tmp = dict()
+                class_tmp["booking_id"] = booked_class["_id"]
+                booked_class = booked_class['class']
+                class_tmp["class_id"] = booked_class["_id"]
+                day = parser.parse(booked_class["classDate"])
+                class_tmp["classDate"] = (day.strftime("%Y-%m-%d"), day.strftime("%A"))
+                class_tmp["classTime"] = booked_class["classTime"]
+                booked_classes.append(class_tmp)
 
         return booked_classes
 
-    def update_candidates(self):
-        self.candidates = []
-        self.candidates = self.generate_candidates()
-
-    def generate_candidates(self) -> Dict[str, List[Tuple[Union[str, List[str]]]]]:
+    def generate_candidates(self) -> List[Tuple[Union[str, List[str]]]]:
         # returns the classes that the user wants to book: has preference AND it is not in its list of scheduled class
         ndays = 8
         candidate_days = self._generate_candidate_days(ndays)
         scheduled_classes = self.get_scheduled_classes()
         candidates = self._filter_days_to_schedule(candidate_days, scheduled_classes)
 
-        candidates_filter = {}
-        for class_type, class_data in candidates.items():
-            if len(class_data) > 0:
-                candidates_filter[class_type] = class_data
+        return candidates
 
-        return candidates_filter
-
-    def get_classes_to_schedule(self, class_type: str, candidates_class: List[Tuple[Union[str, List[str]]]]) -> Tuple[List[
-        Dict[str, Any]], List[Tuple[Union[str, List[str]]]]]:
+    def get_classes_to_schedule(self, candidates_class: List[Tuple[Union[str, List[str]]]]) -> Tuple[
+                                List[Dict[str, Any]], List[Tuple[Union[str, List[str]]]]]:
         # receives the candidates already filtered by the user preferences and filtered by what we already scheduled
         # it then searches for availability for those candidates, if found returns that class information
-
-        url = os.path.join(self.base_url, self.classes_url, class_type)
+        url = os.path.join(self.base_url, self.classes_url, self.class_type)
         r = requests.get(url, headers=self.headers, timeout=self.timeout)
         r.raise_for_status()
-
         bookings = r.json()
-        days_to_filter = [d[0] for d in candidates_class]
 
+        days_to_filter = [d[0] for d in candidates_class]
         filtered_bookings = {}
         classes_to_schedule = []
+        candidates_scheduled = []
         for b in bookings:
             d = parser.parse(b["_id"]).strftime("%Y-%m-%d")
             if d in days_to_filter:
                 filtered_bookings[d] = b
 
-        classes_cancelled = []
         for candidate in candidates_class:
             day = candidate[0]
             candidate_time = self._parse_hour(candidate[2])
             required_spots = 1 + len(candidate[3])
 
-            for real_class in filtered_bookings[day]["classes"]:
-                available_spots = real_class["limit"] - real_class["joinedUsers"]
-                real_class_time = self._parse_hour(real_class["classTime"])
-                if real_class_time > candidate_time:
-                    break
+            if day in filtered_bookings.keys():
+                for real_class in filtered_bookings[day]["classes"]:
+                    available_spots = real_class["limit"] - real_class["joinedUsers"]
+                    real_class_time = self._parse_hour(real_class["classTime"])
+                    if real_class_time > candidate_time:
+                        break
 
-                if real_class_time == candidate_time and available_spots >= required_spots and real_class["active"]:
-                    cancel = False
-                    for attendance in real_class["attendanceList"]:
-                        if attendance["user"] == self.user_id and attendance["status"] == "cancelled":
-                            print(
-                                f"The {class_type} for {day} at {candidate[2]} "
-                                f"was cancelled by user - remove from candidates")
-                            cancel = True
-                            classes_cancelled.append(candidate)
-                            break
-                    if not cancel:
-                        real_class["classDate"] = parser.parse(real_class["classDate"]).strftime("%Y-%m-%d")
-                        classes_to_schedule.append(real_class)
+                    if real_class_time == candidate_time:
+                        cancel = False
+                        for attendance in real_class["attendanceList"]:
+                            if attendance["user"] == self.user_id and attendance["status"] == "cancelled":
+                                self.logger.info(
+                                    f"The {self.class_type} for {day} at {candidate[2]} "
+                                    f"was cancelled by user - remove from scheduling")
+                                cancel = True
+                                candidates_scheduled.append(candidate)
+                                break
+                        if not cancel and available_spots >= required_spots and real_class["active"]:
+                            real_class["classDate"] = parser.parse(real_class["classDate"]).strftime("%Y-%m-%d")
+                            classes_to_schedule.append(real_class)
+                            candidates_scheduled.append(candidate)
 
-        return classes_to_schedule, classes_cancelled
+        candidates_not_available = [c for c in candidates_class if c not in candidates_scheduled]
+        return classes_to_schedule, candidates_not_available
 
     def book_class(self, class_id: str):
         url = os.path.join(self.base_url, "api/class", class_id)
@@ -136,35 +126,30 @@ class BookingUser:
         return candidate_days
 
     def _filter_days_to_schedule(self, candidate_days: Dict[str, List[str]],
-                                 scheduled_classes: Dict[str, List[Dict[str, Any]]]) -> Dict[
-        str, List[Tuple[Union[str, List[str]]]]]:
+                                 scheduled_classes: List[Dict[str, Any]]) -> List[Tuple[Union[str, List[str]]]]:
         # filter days for each class regarding user preferences and classes already scheduled
-        class_candidates = {key: [] for key in self.user_preferences.keys()}
+        class_candidates = []
         now = datetime.datetime.now()
-        for preference_class, preferences in self.user_preferences.items():
-            for day_p in preferences:
-                if day_p[0] in candidate_days.keys():
-                    class_dates = candidate_days[day_p[0]]
-                    class_hours = day_p[1]
-                    candidate = [(class_date, day_p[0], hour, day_p[2])
-                                 for hour in class_hours for class_date in class_dates]
-                    class_candidates[preference_class].extend(candidate)
+
+        for day_p in self.user_preferences:
+            if day_p[0] in candidate_days.keys():
+                class_dates = candidate_days[day_p[0]]
+                class_hours = day_p[1]
+                candidate = [(class_date, day_p[0], hour, day_p[2])
+                             for hour in class_hours for class_date in class_dates]
+                class_candidates.extend(candidate)
 
         # after filtering candidates using the preferences, filter the ones already scheduled
-        filtered_candidates = {key: [] for key in class_candidates.keys()}
-        for preference_class, preferences in class_candidates.items():
+        filtered_candidates = []
+        for candidate in class_candidates:
             scheduled_days = {(day["classDate"][0], day["classTime"].lower()) for day in
-                              scheduled_classes[preference_class]}
-            for candidate in preferences:
-                candidate_date = candidate[0]
-                candidate_weekday = candidate[1]
-                candidate_hour = candidate[2]
-                candidate_friends = candidate[3]
-                preference_datetime = parser.parse(candidate_date) + self._parse_hour(candidate_hour)
+                              scheduled_classes}
+            candidate_date = candidate[0]
+            candidate_hour = candidate[2]
+            preference_datetime = parser.parse(candidate_date) + self._parse_hour(candidate_hour)
 
-                if now < preference_datetime and (candidate_date, candidate_hour) not in scheduled_days:
-                    filtered_candidates[preference_class].append((candidate_date, candidate_weekday,
-                                                                  candidate_hour, candidate_friends))
+            if now < preference_datetime and (candidate_date, candidate_hour) not in scheduled_days:
+                filtered_candidates.append(candidate)
 
         return filtered_candidates
 
@@ -183,13 +168,14 @@ class BookingUser:
         response = r.json()
         self.headers["Authorization"] = f"Bearer {response['token']}"
 
-    def _parse_hour(self, hour_minutes: str):
+    @staticmethod
+    def _parse_hour(hour_minutes: str):
         hour = hour_minutes.split()
         hour_int = int(hour[0].split(":")[0])
         minutes_int = int(hour[0].split(":")[1])
 
         time_of_day = hour[1].lower()
-        if time_of_day == 'pm':
+        if time_of_day == 'pm' and hour_int < 12:
             hour_int = hour_int + 12
 
         return datetime.timedelta(hours=hour_int, minutes=minutes_int)
